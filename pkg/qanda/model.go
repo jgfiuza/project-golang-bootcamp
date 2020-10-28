4dd61a6937294ce76ebb7a5e62eeac0718f5fdb9
João Gabriel Fiuza <joao_fiuza@yahoo.com.br>
2020-10-28 14:10:39 -0300

    feat: qanda modelling

---
+++ pkg/qanda/model.go
@@ -0,0 +1,16 @@
+package qanda
+
+// Question is the model for the 'q' part on q&a
+type Question struct {
+	ID        string `json:"id"`
+	Title     string `json:"title"`
+	Body      string `json:"body"`
+	CreatorID string `json:"created_by"`
+}
+
+// Answer is the model for the 'a' part on q&a
+type Answer struct {
+	QuestionID string `json:"question_id"`
+	Body       string `json:"body"`
+	CreatorID  string `json:"created_by"`
+}
